2db1e1eb71abfa074df3328d1fc26b1f94c0dc24
camden9 <camdenvaughan@knights.ucf.edu>
2021-06-11 17:27:07 -0400

    Finished Build

--- src/GameData.cpp
+++ src/GameData.cpp
@@ -11,15 +11,17 @@ bool GameData::IsGamePlaying(Display* Screen)
 		if (SafeSquaresClicked == TotalSafeSquares)
 		{
 			//Win State
-			Screen->AddStringToArray("YOU WIN!", 5 + Screen->ScreenWidth);
-			Screen->AddStringToArray("Press 'Enter' to continue!", 5 + 3 * Screen->ScreenWidth);
+			Screen->AddStringToArray("YOU WIN!", 10 + 2 * Screen->ScreenWidth);
+			Screen->AddStringToArray("========", 10 + 3 * Screen->ScreenWidth);
+			Screen->AddStringToArray("Press Enter to continue!", 10 + 4 * Screen->ScreenWidth);
 			return false;
 		}
 		else if (GameIsOver == true)
 		{
 			//Lost State
-			Screen->AddStringToArray("You Lose...", 5 + Screen->ScreenWidth);
-			Screen->AddStringToArray("Press 'Enter' to continue!", 5 + 3 * Screen->ScreenWidth);
+			Screen->AddStringToArray("You Lose...", 10 + 2 * Screen->ScreenWidth);
+			Screen->AddStringToArray("===========", 10 + 3 * Screen->ScreenWidth);
+			Screen->AddStringToArray("Press Enter to continue!", 10 + 4 * Screen->ScreenWidth);
 			return false;
 		}
 		return true;
